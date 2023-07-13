4d45cc4551398d20b244fae13f9b53f159b6e9ed
Jinjun Liang <kumkee@users.noreply.github.com>
2023-07-13 23:46:07 +1200

    added nix formatter; formatted

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,30 +1,31 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 -- lemminx: xml
 -- nxls: json
 local servers = {
-  "html",
-  "cssls",
-  "tsserver",
-  "clangd",
-  "elmls",
-  "pyright",
-  "fsautocomplete",
-  "lemminx",
-  "nxls",
-  "csharp_ls",
-  "ltex",
+	"html",
+	"cssls",
+	"tsserver",
+	"clangd",
+	"elmls",
+	"pyright",
+	"fsautocomplete",
+	"lemminx",
+	"nxls",
+	"csharp_ls",
+	"ltex",
+	"nil_ls",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
 --
